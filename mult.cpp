a3eee88db5d86b2af28919f6e0102958fc46181a
yujin0124 <yujin0124@khu.ac.kr>
2020-04-17 12:32:36 +0900

    구구단 수정

--- mult.cpp
+++ mult.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-	int n;
+	int n=9;
 	cout << "숫자를 입력해주세요 (1~9)" << endl;
-	
+	cin >> n;
 
-	for (int i = 1; i <= 9; i++) {
+	for (int i = 1; i <= n; i++) {
         for(int j=1; j<=9; j++)
             cout <<i<< " * " << j << " = " << i * j << endl;
         cout<<'\n';
